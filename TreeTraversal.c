ee2884b712e53680aa8788d7665ff7dfc09bf9d8
chhandasaha <43537144+chhandasaha@users.noreply.github.com>
2021-01-20 21:37:20 +0530

    added comment in TreeTraversal.c

--- TreeTraversal.c
+++ TreeTraversal.c
@@ -27,8 +27,11 @@ Node* newNode(int data)
 //insert a node in the tree
 Node* insertNode(Node* node, int data)
 {
+	//if root is null then newNode will be the root 
 	if (node == NULL)
 		return newNode(data);
+		
+	//if data is less than the node's key, insert it in left sub tree, else in right subtree	
 	if(data < node->key)
 		node->left = insertNode(node->left, data);
 	else if(data> node->key)
@@ -74,24 +77,31 @@ void printPostOrder(Node* node)
 int main()
 {
 	int i, arr[MAX], N;
+	//enter number of element in the tree
 	scanf("%d", &N);
+	// give the array input
 	scanf("%d", arr);
+	//if number of element is less than or equal to 1, print that
 	if (N <= 1)
 	{
 		printf("%d", N);
 		return 0;
 	}
+	// insert node at the o-th position of the array, and return the root
 	root = insertNode(root, *(arr));
 	for(i = 0; i<N-1; i++)
 	{
+		//scan the ith element and insert it in the tree.
 		scanf("%d", (arr+i));
 		insertNode(root, *(arr+i));
 	}
-	
+	//print in preorder
 	printPreOrder(root);
 	printf("\n");
+	//print in inorder
 	printInOrder(root);
 	printf("\n");
+	//print in postOrder
 	printPostOrder(root);
 	
 	return 0;
